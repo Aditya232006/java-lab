ae93f7ce3f350662ddfb6640cc771f1a484a77aa
Aditya232006 <adityarathi232006@gmail.com>
2026-03-09 09:41:57 +0530

    Update StackInterfaceDemo.java

--- JAVA-LAB-SY-3-main/Java-Lab/Experiment-4/StackInterfaceDemo.java
+++ JAVA-LAB-SY-3-main/Java-Lab/Experiment-4/StackInterfaceDemo.java
@@ -1,5 +1,5 @@
-// Name   : Atharv Kate
-// Roll No: 15
+// Name   : Aditya Rathi 
+// Roll No: 39
 // Experiment 3: Implementation of stack/queue operations using Interface
 // Program 1: Stack Operations using Interface
 
