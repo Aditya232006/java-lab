52c4acbb0401ee8160b287f8c75d3a661a815b2a
Aditya232006 <adityarathi232006@gmail.com>
2026-03-09 09:45:56 +0530

    Update BankAccountDemo.java

--- JAVA-LAB-SY-3-main/Java-Lab/Experiment-6/BankAccountDemo.java
+++ JAVA-LAB-SY-3-main/Java-Lab/Experiment-6/BankAccountDemo.java
@@ -1,5 +1,5 @@
-// Name   : Atharv Kate
-// Roll No: 15
+// Name   : Aditya Rathi 
+// Roll No: 39
 // Experiment 5: Implementation of Exception Handling
 // Program: BankAccount with user-defined exceptions
 //          LowBalanceException and NegativeNumberException
