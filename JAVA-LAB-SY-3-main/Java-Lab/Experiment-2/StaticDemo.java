802c0ba3015609ba6167b8775a264b22bef7cbc6
Aditya232006 <adityarathi232006@gmail.com>
2026-03-09 09:38:24 +0530

    Update StaticDemo.java

--- JAVA-LAB-SY-3-main/Java-Lab/Experiment-2/StaticDemo.java
+++ JAVA-LAB-SY-3-main/Java-Lab/Experiment-2/StaticDemo.java
@@ -1,5 +1,5 @@
-// Name   : Atharv Kate
-// Roll No: 15
+// Name   : Aditya Rathi 
+// Roll No: 39
 // Experiment 1: Implementation of a problem statement using class and object
 // Program 3: Demonstrate static variables, static blocks, and static methods
 
